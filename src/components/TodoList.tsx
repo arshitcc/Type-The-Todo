6dc1026290d999b8d57ea138f7273482f890bc79
arshitcc <arshitcc@gmail.com>
2024-10-23 03:37:32 +0530

    fix(todo) : edit todo feature #1

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -8,6 +8,7 @@ export const TodoList = () => {
   const toggleTodo = useTodoStore((state) => state.toggleTodo);
   const removeTodo = useTodoStore((state) => state.removeTodo);
   const setCurrentTodo = useTodoStore((state) => state.setCurrentTodo);
+  const currentTodo = useTodoStore((state) => state.currentTodo);
 
   return (
     <div className="mt-12 p-4 bg-zinc-500 rounded-xl">
@@ -24,13 +25,13 @@ export const TodoList = () => {
                 <p className="break-words whitespace-normal">{todo.description}</p>
               </div>
               <div className="row-span-1 col-span-2 grid grid-cols-3 gap-2 items-center">
-                <Button onClick={() => setCurrentTodo?.(todo)}>
+                <Button onClick={() => setCurrentTodo?.(todo)} disabled={currentTodo?._id ===todo._id}>
                   <PencilRulerIcon/>
                 </Button>
-                <Button onClick={() => toggleTodo(todo._id)} className={`  ${todo.isCompleted? 'bg-zinc-800 hover:bg-zinc-800' : 'bg-green-500 hover:bg-green-500'}`}>
+                <Button onClick={() => toggleTodo(todo._id)} className={`  ${todo.isCompleted? 'bg-zinc-800 hover:bg-zinc-800' : 'bg-green-500 hover:bg-green-500'}`} disabled={currentTodo?._id ===todo._id}>
                   {todo.isCompleted ? <RotateCcwIcon/> : <SquareCheckBigIcon/>}
                 </Button>
-                <Button onClick={() => removeTodo(todo._id)} variant="destructive">
+                <Button onClick={() => removeTodo(todo._id)} variant="destructive" disabled={currentTodo?._id ===todo._id}>
                   <CircleXIcon/>
                 </Button>
               </div>
